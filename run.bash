99fdb5c87e724994953633fc718bbd8226401697
borish3198 <borish3198@gmail.com>
2023-03-17 10:58:10 +0900

    9th

---
+++ run.bash
@@ -0,0 +1,2 @@
+./gradlew build
+java -jar build/libs/evaluation-0.0.1-SNAPSHOT.jar
--- src/main/java/hackathon/evaluation/v1/controller/TestController2.java
+++ src/main/java/hackathon/evaluation/v1/controller/TestController2.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(originPatterns = "http://localhost:8080")
+@CrossOrigin(originPatterns = "http://121.135.181.61:8080")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/test2")
