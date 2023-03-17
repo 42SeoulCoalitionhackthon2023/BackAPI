baf7e01dd8ecba87da779cdf52d5c674b7cb6649
borish3198 <borish3198@gmail.com>
2023-03-17 11:03:46 +0900

    10th

--- run.bash
+++ run.bash
@@ -1,2 +1,5 @@
+git add -A
+git stash
+git pull
 ./gradlew build
 java -jar build/libs/evaluation-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/hackathon/evaluation/v1/controller/TestController4.java
@@ -0,0 +1,26 @@
+package hackathon.evaluation.v1.controller;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hackathon.evaluation.v1.domain.dto.TestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(originPatterns = "http://121.135.181.61:3000")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test2")
+public class TestController4 {
+
+    private final ObjectMapper objectMapper;
+
+    @GetMapping()
+    TestDto test() throws JsonProcessingException {
+        TestDto greeting = new TestDto();
+        greeting.setText("hello world");
+
+        return greeting;
+    }
+}
